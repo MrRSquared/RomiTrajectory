f114ae983810ef067014b1f98ef7683adb2e23d5
MrRSquared <jrothritchie@hdsd.org>
2021-03-23 22:56:22 -0400

    cleaned up a bit more

--- src/main/java/frc/robot/commands/FindNewTrajectoryRuntime.java
+++ src/main/java/frc/robot/commands/FindNewTrajectoryRuntime.java
@@ -24,6 +24,7 @@ import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.Drivetrain;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+
 // information, see: https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class FindNewTrajectoryRuntime extends CommandBase {
   /** Creates a new PlotTrajectory. */
